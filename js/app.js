aacb059478eb2b411ff58c870c8c7fc725b5b794
chamer79 <chamer79@gmail.com>
2024-08-06 13:41:21 -0400

    added psuedocode

--- js/app.js
+++ js/app.js
@@ -1,1 +1,62 @@
-console.log("sanity check")
+/*-------------------------------- Constants --------------------------------*/
+// Step1. Create an wordBank array of a minimum of 13 words.
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+// Step 2. Define required variables to track the state of the game. -> player(do I need a player variable for a 1 person game?), board, winner, randomWord, guessedWord
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+// Step 3. Store cache element refernces. -> .keyboard(?), .key-row, .word-display, .dispaly-row(?), #delete, #enter
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+// Step 4. Upon loading, the game state should be initialized, and a function should be called to render this game state
+    // a. create and init()
+    // b. set board to and array with 30 empty string - 5x6
+    // c. set winner to false
+    // d. set randomWord to randomly assign a word from wordBank -> Math.random() & Math.floor()?
+    // e. set gessedWord to an empty string
+
+// Step 5. The state of the game should be rendered to the user.
+    // a. create render function
+    // b.  create updateBoard function
+        // c. loop over board & keyboard for each el -> 1)use the current idx to access the corresponding tiles & style to correspond to the correct / incorrect letter in each tile
+             // c1. if the tile has the correct letter && in the correct tile index: tile = #079855, letter = white, +tile animation 
+             // c2. if the tile has the correct letter && ! in the correct tile index: tile = #daa520, letter = white, +tile animation
+             // c.3 if the tile ! have the correct letter && ! inthe correct tile index: tile = #616060, letter = white, +tile animation
+    // d. create updateMessage function
+        // e. render a message if the word entered is: 
+            // e1. too short -> < randomWord.length 
+            // e2. too long -> > randomWord.length
+        // f. render a message that if the player won or lost -> ?? would it be better to place this message in checkForWinner()??
+    // g. call render() in the init()    
+
+// Step 7. create a reset functionality
+
+
+        
+/*----------------------------- Event Listeners -----------------------------*/       
+// Step 6. Handle a player clicking a letter with a handleClick function -> THIS IS FOR KEYBOARD .
+    // a. create a handleClick function
+    // b. set up an event listener to each square in the keyboard w/ a loop. set up event listerner to respond to 'click' & should call handleClick
+        // b1. Have a backspace key to delete letters. -> .pop
+        // b2. Have an enter key to submit the guess. render word in the current blank row
+    // c(?). get the index for an id assigned to the target el in the HTML - assign this to a const called keyIndex
+    // d. if winner = true return out of handleClick
+
+    // Step 6.1a: create a currentTurn function that accepts a parameter of index??? -> will probably need an interation and if/else(?)
+    // if current turn/ row is empty then populate with current guessed word
+    //if row ! empty, move to the next turn / row    
+        // 6.1b: update board so that it is = to the current guessedWord of the turn
+        // 6.1c: call currentTurn() in handleClick
+    
+    // Step 6.2a: create a checkForWinner()
+        // 6.2b: if there is a winner / randomWord = wordEntered - message: "you won!" & return out of function
+        // call checkForWinner() in handleClick
+
+    // Step 6.3: call the render() in handleClick
+           
