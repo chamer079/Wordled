65bc27d4ff937a06c115160c65f5b2d1f971ac27
chamer79 <chamer79@gmail.com>
2024-08-08 16:49:06 -0400

    working on currrentTurn(). currentAttempt() is now currentTurn(). added code to updateMessage().

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@ function init(){
     ]
     winner = false
     randomWord = wordBank[(Math.floor(Math.random() * wordBank.length))]
-    // console.log("randomWord test:", randomWord)
+    console.log("randomWord test:", randomWord)
     guessedWord = ["", "", "", "", ""]
     attempts = 6
 } 
@@ -106,20 +106,19 @@ function updateBoard(){
     
 
     // c. loop over board & keyboard for each el -> 1)use the current idx to access the corresponding tiles & style to correspond to the correct / incorrect letter in each tile
-    
-    
-    
-            // c1. if the tile has the correct letter && in the correct tile index: tile = #079855, letter = white, +tile animation 
-            // c2. if the tile has the correct letter && ! in the correct tile index: tile = #daa520, letter = white, +tile animation
-            // c.3 if the tile ! have the correct letter && ! inthe correct tile index: tile = #616060, letter = white, +tile animation
+        // c1. if the tile has the correct letter && in the correct tile index: tile = #079855, letter = white, +tile animation 
+        // c2. if the tile has the correct letter && ! in the correct tile index: tile = #daa520, letter = white, +tile animation
+        // c.3 if the tile ! have the correct letter && ! inthe correct tile index: tile = #616060, letter = white, +tile animation
 }
 
 
 function updateMessage(){  
     // would it be better to place this message in checkForWinner()??
-    if(winner === false){
+    if(winner === false && attempts === 0){
         messageEl.textContent = `Better luck next time.The correct word was ${randomWord} Want to try again?`
         // console.log("winner = false")
+    } else if(winner === false && attempts !== 0 && attempts <= 6){
+        messageEl.textContent = `You have ${attempts} turns remaining.`
     } else{
         messageEl.textContent = "Congratulations! You guessed the word. Want to try again?"
         // console.log("winner = true")
@@ -130,30 +129,32 @@ function updateMessage(){
     }   else if (guessedWord.length > 5){
         messageEl.textContent = "The word is too long."
     }
-    // e. render a message if the word entered is: 
-        // e1. too short -> < randomWord.length 
-        // e2. too long -> > randomWord.length
 }
 
 function placeLetter(index){  // will probably need an interation and if/else(?)
-    displayBoard[index] = currentAttempt  // 6.1b: update displayBoard arr ar the index to that it is = to the current value of attempts
+    displayBoard[index] = attempts  
+
+    // console.log("currentAttempt test", attempts)
 }
         
-// **** Maybe combine both currentAttempt() & checkForWinner -> both () will more than likely need to iterate through each row to see if there are any blank spaces? -> practicing DRY codeing? Would it be better to compartmentalize into different functions? **//
-function currentAttempt(){
-    // if current attempt is empty then populate with current guessed word
-    displayBoard.forEach((row, idx) => {
-      if(winner === false){
-        if(row[idx] === "" && attempts <= 6){
-            //
-            
-            attempts--
+// **** Maybe combine both currentTurn() & checkForWinner -> both () will more than likely need to iterate through each row to see if there are any blank spaces? -> practicing DRY codeing? Would it be better to compartmentalize into different functions? **//
 
-        }
-      } else {
-        return
-      }
-    })
+function currentTurn(){
+    // if current turn is empty then populate with current guessed word
+    if(winner === false && attempts !== 0){
+        displayBoard.forEach((row, idx) => {
+            if(row[idx] === ""){
+                displayRowEls[idx].textContent = guessedWord
+
+                attempts--
+
+                console.log("guessedWord Test", guessedWord)
+            } else{
+                return
+            }
+        })
+    }
+
 
     //if row ! empty, move to the next turn / row    
        // 6.2b: update board so that it is = to the current guessedWord of the turn
@@ -182,6 +183,7 @@ function handleClick(event){
     }   
     
     placeLetter(event.target.id)
+    currentTurn()
     
 }
 
